bbfcdc596acd94d2e1d1edf10c975cc45ce23012
Benny623 <bmilner88@gmail.com>
2022-07-21 00:01:31 -0700

    Cleaned up some strings

--- GH5 PS3 Controller/code.py
+++ GH5 PS3 Controller/code.py
@@ -38,7 +38,7 @@ print('Scanning i2c bus')
 devices = i2c.scan()
 
 if len(devices) == 0:
-    print("No i2c device !")
+    print("No i2c device")
 else:
     print('i2c devices found:', len(devices))
 
